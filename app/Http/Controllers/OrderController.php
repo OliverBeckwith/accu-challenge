2b94cd0457e7eaee95a27572dc0576ab307df520
Ollie Beckwith <oliver.beckwith1@gmail.com>
2023-10-05 15:49:30 +0100

    Enforce auth on order controller

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function showOrders()
     {
         $orders = Order::all();
