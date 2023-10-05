5c86c28a43ca9456c0ee47d715c07a7d89acb371
Ollie Beckwith <oliver.beckwith1@gmail.com>
2023-10-05 11:46:40 +0100

    Add OpenAI to generate bot names

--- app/Services/BotNameGenerator.php
+++ app/Services/BotNameGenerator.php
@@ -4,9 +4,19 @@ namespace App\Services;
 
 use App\Models\Order;
 use App\Models\Product;
+use Illuminate\Support\Env;
+use OpenAI\Client;
+use OpenAI;
 
 class BotNameGenerator
 {
+    private Client $client;
+
+    public function __construct()
+    {
+        $this->client = OpenAI::client(env("OPENAI_API_KEY"));
+    }
+
     /** Retrieve the most prevalent category from the order items */
     protected function getOrderCategory(Order $order)
     {
@@ -29,12 +39,33 @@ class BotNameGenerator
         return array_keys($counts)[0];
     }
 
+    /** Use OpenAI GPT 3.5 to generate an amusing bot name */
+    protected function generateBotNameWithOpenAI(string $category): string|null
+    {
+        $chatResponse = $this->client->chat()->create([
+            'model' => 'gpt-3.5-turbo',
+            'prompt' => "Generate a single word amusing robot name loosely based on the following category: '$category'" . PHP_EOL
+                . "(Examples: 'Boltinator', 'Nutbuster', 'Washzilla')" . PHP_EOL
+                . "Your response should only be a single word: the robot name.",
+            'temperature' => 0.7, //Allow creativity
+        ]);
+        $botName = null;
+        foreach ($chatResponse->choices as $result) {
+            if ($result->finishReason === 'stop') {
+                $botName = trim($result->message->content);
+            }
+        }
+        return $botName;
+    }
+
     /** Generate a bot name for the given order */
     public function generate(Order $order)
     {
         $orderCategory = $this->getOrderCategory($order);
 
-        //TODO: Replace placeholder below with generated bot name
-        return "Nutzilla";
+        $fallbackBotName = "Boltinator";
+        $openAiBotName = $this->generateBotNameWithOpenAI($orderCategory);
+
+        return $openAiBotName ?? $fallbackBotName;
     }
 }
